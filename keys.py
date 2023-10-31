68f8b85236c294873377b2ac1282b2a5ad35130e
Alfred Holmes <holmesey55@gmail.com>
2023-10-31 12:44:32 +0000

    removed keys from repo

--- keys.py
+++
@@ -1,6 +0,0 @@
-API_KEY = "72Tyc3Arr8A8uZ4dBUnoIbIn"
-USER_ID = "10993132"
-
-PDF_DIRECTORY = '/home/alfred/Sync/Notability'
-ZOTERO_ROOT = '/home/alfred/Sync/'
-SEARCH_EXCLUDE = 'attachments:References'
